e5b57a61322098a3188c1800bfadc6039d24f152
Matthew Evans <git@ml-evs.science>
2025-09-29 18:22:04 +0100

    Add vault-pass script that uses bw and repo name

---
+++ .vault-pass.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script retrieves the password for the current git repository from a Bitwarden Vault.
+# If bw is not available, it should error out
+set -e
+bw get password "$(git remote get-url origin | awk '{split($0, a, "/"); print a[length(a)]}')"

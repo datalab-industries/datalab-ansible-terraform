596befc0ffd20e42e057085bc0a8e5079f10f780
Matthew Evans <git@ml-evs.science>
2025-11-16 11:57:23 +0000

    Allow which templates are copied across to be determined by env var

--- sync-ansible-upstream.sh
+++ sync-ansible-upstream.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e -u -o pipefail
+# Check for DATALAB_ANSIBLE_TEMPLATE_DIR environment variable
+# If set, use that as the TEMPLATE_ROOT directory
+# Otherwise, use "src" as the default
+if [ -n "${DATALAB_ANSIBLE_TEMPLATE_DIR:-}" ]; then
+    TEMPLATE_ROOT="$DATALAB_ANSIBLE_TEMPLATE_DIR"
+else
+    TEMPLATE_ROOT="src"
+fi
+
 commit=$(cd src/datalab-ansible-terraform && git describe --tags)
-rsync --exclude vaults --exclude inventory.yml -avr src/datalab-ansible-terraform/sync-ansible-upstream.sh src/datalab-ansible-terraform/Makefile src/datalab-ansible-terraform/.vault-pass.sh src/datalab-ansible-terraform/README.md src/datalab-ansible-terraform/requirements.txt src/datalab-ansible-terraform/ansible .
+rsync --exclude vaults --exclude inventory.yml -avr "$TEMPLATE_ROOT/datalab-ansible-terraform/sync-ansible-upstream.sh" "$TEMPLATE_ROOT/datalab-ansible-terraform/Makefile" "$TEMPLATE_ROOT/datalab-ansible-terraform/.vault-pass.sh" "$TEMPLATE_ROOT/datalab-ansible-terraform/README.md" "$TEMPLATE_ROOT/datalab-ansible-terraform/requirements.txt" "$TEMPLATE_ROOT/datalab-ansible-terraform/ansible" .
+chmod u+x ./.vault-pass.sh
 git add -p ansible
 git add src/datalab-ansible-terraform
 git add $(git ls-files ansible --others --exclude-standard)

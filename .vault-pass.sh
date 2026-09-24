5926ffee67883f785ce4156f8bb6aca311ca8f58
Matthew Evans <git@ml-evs.science>
2026-09-24 14:39:03 +0100

    Look up rbw password from <org>/<repo> name as well as just <repo>

--- .vault-pass.sh
+++ .vault-pass.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # This script retrieves the password for the current git repository from a Bitwarden Vault using rbw.
+# The entry is looked up as "<org>/<repo>" (e.g., "datalab-industries/datalab-ansible-terraform"),
+# falling back to just "<repo>" for entries created before the org was included.
 # If rbw is not available, it should error out
 set -e
-rbw get "$(git remote get-url origin | awk '{split($0, a, "/"); print a[length(a)]}')" || { read -sp "Password or Bitwarden CLI not found. Enter vault password: " password; echo "$password"; }
+remote="$(git remote get-url origin | sed -E 's#^.*[:/]([^/:]+/[^/]+)$#\1#')"
+rbw get "$remote" 2>/dev/null || rbw get "${remote#*/}" || { read -sp "Password or Bitwarden CLI not found. Enter vault password: " password; echo "$password"; }
